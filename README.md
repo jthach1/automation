73cd08f236e510f66b51c1907c7bc99d438c77bb
John Thach <johnny6783@gmail.com>
2024-06-12 11:15:38 -0700

    fix test and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # automation
+
+Here is where I examine TS automation with Cypress and Playwright
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('search someting on Youtube', () => {
     cy.visit('https://youtube.com');
     cy.get('input#search').type('a search{enter}');
-    cy.get('button').should('be.a')
+    cy.get('div#contents').should('be.visible').and('be.');
   })
 })
